c46d0dd7cb7ab4af281fc085748bc4956aa27e6f
Nicholas Iorio <niorio1@gmail.com>
2015-03-15 23:27:21 -0400

    used built-in validators to check for like uniqueness

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,13 +1,7 @@
 class Like < ActiveRecord::Base
   validates :user, :photo, presence: true
-  validate :unique_like
+  validates_uniqueness_of :user_id, scope: :photo_id
   belongs_to :user
   belongs_to :photo
   
-  private
-  def unique_like
-    if Like.where(user_id: user_id, photo_id: photo_id).exists?
-      errors.add(:user, "cannot like something multiple times")
-    end
-  end
 end
