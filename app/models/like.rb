7d0ea85ea2ed657c8605dddea2dbdf75efc2165d
Nicholas Iorio <niorio1@gmail.com>
2015-03-16 19:38:38 -0400

    like validation message

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ActiveRecord::Base
   validates :user, :photo, presence: true
-  validates_uniqueness_of :user_id, scope: :photo_id
+  validates_uniqueness_of :user, scope: :photo, message: "cannot like something more than once"
   belongs_to :user
   belongs_to :photo
   
