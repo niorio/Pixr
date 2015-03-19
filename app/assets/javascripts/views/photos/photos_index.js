fe0fd1ba8fa947ca87ae3a7116024e4d6eb7d24d
Nick Iorio <niorio1@gmail.com>
2015-03-19 09:52:03 -0400

    messing with compositeview

--- app/assets/javascripts/views/photos/photos_index.js
+++ app/assets/javascripts/views/photos/photos_index.js
@@ -1,4 +1,4 @@
-Pixr.Views.PhotosIndex = Backbone.View.extend({
+Pixr.Views.PhotosIndex = Backbone.CompositeView.extend({
 
   template: JST['photos/index'],
 
@@ -8,16 +8,17 @@ Pixr.Views.PhotosIndex = Backbone.View.extend({
 
   initialize: function (options) {
     this.listenTo(this.collection, 'sync add', this.render);
-
-    this.subviews = [];
   },
 
   render: function () {
     var that = this;
+    var photoItemView;
     this.$el.empty();
 
     this.collection.each(function(photo) {
-      that.appendPhoto(photo);
+      photoItemView = new Pixr.Views.PhotoIndexItem({ model: photo });
+      that.addSubview(that.el, photoItemView)
+      photoItemView.render();
     })
 
     this.$el.justifiedGallery({
@@ -29,18 +30,18 @@ Pixr.Views.PhotosIndex = Backbone.View.extend({
     return this;
   },
 
-  appendPhoto: function (photo) {
-    var photoItemView = new Pixr.Views.PhotoIndexItem({ model: photo });
-    this.$el.append(photoItemView.render().$el);
-    this.subviews.push(photoItemView);
-  },
-
-  remove: function () {
-    _.each(this.subviews, function(subview){
-      subview.remove();
-    });
-    this.subviews = [];
-    Backbone.View.prototype.remove.call(this);
-  }
+  // appendPhoto: function (photo) {
+  //   var photoItemView = new Pixr.Views.PhotoIndexItem({ model: photo });
+  //   this.$el.append(photoItemView.render().$el);
+  //   this.subviews.push(photoItemView);
+  // },
+  //
+  // remove: function () {
+  //   _.each(this.subviews, function(subview){
+  //     subview.remove();
+  //   });
+  //   this.subviews = [];
+  //   Backbone.View.prototype.remove.call(this);
+  // }
 
 });
