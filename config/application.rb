24736c0f070ad8ef92aba70045f49529749ca626
Nicholas Iorio <niorio1@gmail.com>
2015-03-21 11:32:14 -0400

    s3 now uses https, no more mixed content warnings

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,8 @@ module Pixr
         :bucket => ENV["S3_BUCKET"],
         :access_key_id => ENV["S3_ACCESS_KEY_ID"],
         :secret_access_key => ENV["S3_SECRET_ACCESS_KEY"]
-      }
+      },
+      :s3_protocol => :https
     }
 
 
